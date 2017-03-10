c925bada229ce88865dc18c30de115c85bb8bc85
KUO Yi Chun <yckuo5@gpu9.cse.cuhk.edu.hk>
2017-03-11 00:22:39 +0800

    add models

---
+++ train_global_context.sh
@@ -0,0 +1,2 @@
+nohup python exp-referit/exp_train_referit_seg_lowres.py 6 > exp-referit/log/train_referit_seg_lowres_global_context.log
+nohup python exp-referit/exp_train_referit_seg_highres.py 6 > exp-referit/log/train_referit_seg_highres_global_context.log
